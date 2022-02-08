6363959583eab243bcabd99053ba76460052a72b
Geetha Medisetty <geetha.medisetty@gmail.com>
2022-02-07 21:39:18 -0800

    first assignment

---
+++ greetings/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Coding Dojo");
+		System.out.println("I am 100 years old");
+		System.out.println("My hometown is Burbank, CA");
+    
+	}
+}
+
+
+
+
